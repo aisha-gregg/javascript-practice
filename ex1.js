01d15ef4bb0687443a84f16bc3fa50562f8dbb85
Aisha Gregg <aishagregg@gmail.com>
2019-09-08 21:54:08 +0200

    exercise 1 javascript

---
+++ ex1.js
@@ -0,0 +1,2 @@
+const radius = prompt("Enter a circumference radius");
+alert(`The area of a cirumference whose radius is ${radius} is ${radius ** 2 * 3.14}`);
---
+++ ex2.js
@@ -0,0 +1,5 @@
+const input1 = prompt(Enternumber1);
+const input2 = prompt(Enternumber2);
+const a = document.getElementById(input1).value;
+const b = document.getElementById(input2).value;
+alert(a + b, a - b, a * b);
